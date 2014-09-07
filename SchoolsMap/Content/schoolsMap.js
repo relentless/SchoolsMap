95347f22d40f98c4959971404d7784b8a46bfd61
Grant Crofton <grantac@hotmail.com>
2014-09-07 15:28:03 +0100

    Moved JS into script

---
+++ SchoolsMap/Content/schoolsMap.js
@@ -0,0 +1,31 @@
+﻿function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(53.7996388, -1.5491220999999768),
+        zoom: 14
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    $.ajax({
+        url: "http://localhost:3579/schools",
+        type: 'GET',
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+
+            for (index = 0; index < data.length; ++index) {
+                var school = data[index]
+                var myLatlng = new google.maps.LatLng(school.Lat, school.Lon);
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    map: map,
+                    title: school.Name + "\nRank " + school.Rank,
+                    icon: 'Content/1stQuartile.png'
+                });
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+
+}
--- SchoolsMap/IndexModule.fs
+++ SchoolsMap/IndexModule.fs
@@ -1,6 +1,14 @@
 ﻿namespace SchoolsMap
 
 open Nancy
+open Newtonsoft.Json
+
+type School = {
+    Name: string;
+    Lat: decimal;
+    Lon: decimal;
+    Rank: float
+}
 
 type IndexModule() as x =
     inherit NancyModule()
@@ -9,13 +17,17 @@ type IndexModule() as x =
 
     do x.Get.["/schools"] <- fun _ -> 
 
-        "{
-            \"Lat\": 53.7996388, 
-            \"Lon\": -1.5491220999999768,
-            \"Name\": \"Leeds School\",
-            \"Type\": \"secondary\",
-            \"Private\": true,
-            \"Religious\": false,
-            \"Rank\": 0.75
-         }" :> obj
+        let json = 
+            JsonConvert.SerializeObject(
+                [
+                    { Name = "Leeds School";
+                      Lat = 53.7996388m;
+                      Lon = -1.5491220999999768m;
+                      Rank = 0.75 };
+                    { Name = "Other School";
+                      Lat = 53.787547m;
+                      Lon = -1.543452m;
+                      Rank = 0.75 }
+                 ])
 
+        json :> obj
