52fef982be7df757d8ad52e91ae33056ec6c217b
Grant Crofton <grantac@hotmail.com>
2014-09-12 14:13:25 +0100

    Added all icons

--- SchoolsMap/IndexModule.fs
+++ SchoolsMap/IndexModule.fs
@@ -30,15 +30,37 @@ type School = {
 type IndexModule() as x =
     inherit NancyModule()
 
-    let getIcon = function
-        | { SchoolPre.Type = "Primary" } -> "Primary.png"
-        | { SchoolPre.Rank = rank; SchoolPre.Private = true } when rank > 0.75 -> "1stQuartile_$.png"
-        | { SchoolPre.Rank = rank; SchoolPre.Private = true } when rank <= 0.75 -> "2ndQuartile_$.png"
-        | { SchoolPre.Rank = rank } when rank > 0.75 -> "1stQuartile.png"
-        | { SchoolPre.Rank = rank } when rank <= 0.75 -> "2ndQuartile.png"
+    let sixth = (1.0/6.0)
+
+    let rankPart = function
+        | rank when rank >= sixth*5.0 -> "1"
+        | rank when rank >= sixth*4.0 -> "2"
+        | rank when rank >= sixth*3.0 -> "3"
+        | rank when rank >= sixth*2.0 -> "4"
+        | rank when rank >= sixth*1.0 -> "5"
+        | _ -> "6"
+
+    let typePart = function
+        | "Secondary" -> "s"
+        | "Primary" -> "p"
+        | x -> failwith "Unrecognised school type: " + x
+
+    let trueFalsePart = function
+        | true -> "t"
+        | false -> "f"
+
+    let religiousPart _ = "f"
+
+    let getIcon (school:SchoolPre) = 
+        sprintf "%s_%s_%s_%s_%s.png"
+            (school.Type |> typePart)
+            (school.Rank |> rankPart)
+            (school.Private |> trueFalsePart)
+            (school.Religious |> trueFalsePart)
+            (school.Catholic |> trueFalsePart)
 
     let loadSchoolData() = 
-        File.ReadAllLines("""C:\Programming\SchoolsMap\SchoolsMap\data\school_locations.csv""").[1..]
+        File.ReadAllLines(__SOURCE_DIRECTORY__ + """\data\school_locations.csv""").[1..]
         |> Array.map (fun (line:string) -> line.Split(','))
         |> Array.map (fun school -> {Name = school.[0]; Lat = decimal school.[1]; Lon = decimal school.[2]; Rank = float school.[3]; Score = float school.[4]; Private = Convert.ToBoolean(school.[5]); Religious = Convert.ToBoolean(school.[6]); Catholic = Convert.ToBoolean(school.[7]); Type = school.[8] })
         |> Array.map (fun school -> {Name = school.Name; Lat = school.Lat; Lon = school.Lon; Rank = school.Rank; Score = school.Score; Type = school.Type; Icon = school |> getIcon})
