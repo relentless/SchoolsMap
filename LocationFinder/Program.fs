6abccff53bdc0489cd62789ec30b7e07e99a3a8c
Grant Crofton <grantac@hotmail.com>
2014-09-13 00:35:07 +0100

    Tweaked lookup, searched all schools

--- LocationFinder/Program.fs
+++ LocationFinder/Program.fs
@@ -11,9 +11,20 @@ let lookup name =
     let results = Location.Parse(jsonResults)
     results.Results
 
+let lat (location:Location.Result) =
+    location.Geometry.Location.Lat
+
+let lon (location:Location.Result) =
+    location.Geometry.Location.Lng
+
 let showGetResults school (locations:Location.Result []) index =
-    printfn "%s Lat: %f Lon: %f" school locations.[index].Geometry.Location.Lat locations.[index].Geometry.Location.Lng
+    printfn "%s Lat: %f Lon: %f" school (locations.[index] |> lat) (locations.[index] |> lon)
     (locations.[index].Geometry.Location.Lat, locations.[index].Geometry.Location.Lng)
+
+let (|Distinct|) (locations:Location.Result []) =
+    locations
+    |> Seq.countBy( fun location -> System.Math.Round ((location |> lat) + (location |> lon), 2) )
+    |> Seq.length
 
 [<EntryPoint>]
 let main _ = 
@@ -36,20 +47,21 @@ let main _ =
             let name = schoolInfo.[0].Replace("'","")
             let locations = lookup name
 
-            let (lat,lon) = 
-                match locations.Length with
-                | 0 -> printfn "\n**No locations found for %s**\n" name
-                       (0M, 0M)
-                | 1 -> showGetResults name locations 0
+            let (latitude,longitude) = 
+                match locations with
+                | Distinct 0 -> 
+                        printfn "\n**No locations found for %s**\n" name
+                        (0M, 0M)
+                | Distinct 1 -> showGetResults name locations 0
                 | _ -> 
-                    let choices = locations |> Array.mapi (fun index location -> sprintf "%i %s\n" index location.FormattedAddress)
-                    printfn "\nMore than one location was found for '%s'.  Please enter selection or X for none:\n%s" name (choices |> Array.reduce (+))
+                    let choices = locations |> Array.mapi (fun index location -> sprintf "%i: %s %f %f\n" index location.FormattedAddress (location |> lat) (location |> lon))
+                    printfn "\nMore than one location was found for '%s'.\nPlease enter selection or X for none:\n\n%s" name (choices |> Array.reduce (+))
 
                     match Console.ReadKey().KeyChar.ToString() with
                     | "x" -> (0M, 0M)
                     | selectedIndex -> showGetResults name locations (int selectedIndex)
 
-            sprintf "%s,%f,%f,%f,%f,%s,%s,%s,%s" name lat lon (float schoolInfo.[2]) (float (schoolInfo.[3].Replace("%", ""))) schoolInfo.[4] schoolInfo.[5] schoolInfo.[6] schoolInfo.[7])
+            sprintf "%s,%f,%f,%f,%f,%s,%s,%s,%s" name latitude longitude (float schoolInfo.[2]) (float (schoolInfo.[3].Replace("%", ""))) schoolInfo.[4] schoolInfo.[5] schoolInfo.[6] schoolInfo.[7])
 
     let outputIncludingHeader = outputLines |> Array.append [|"name, latitude, longitude, rank, score, private, religious, catholic, type"|]
     
