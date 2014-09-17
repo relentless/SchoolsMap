d5f9aad8df6ac4b44d37ade437b8487939e7dce7
Grant Crofton <grantac@hotmail.com>
2014-09-17 08:54:53 +0100

    Added ASP version for Azure

---
+++ LeedsSchoolsMap/Content/schoolsMap.js
@@ -0,0 +1,31 @@
+﻿function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(53.7996388, -1.5491220999999768),
+        zoom: 14
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    $.ajax({
+        url: "http://localhost:34298/schools",
+        type: 'GET',
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+
+            for (index = 0; index < data.length; ++index) {
+                var school = data[index]
+                var myLatlng = new google.maps.LatLng(school.Lat, school.Lon);
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    map: map,
+                    title: school.Name + "\nType: " + school.Type + "\nRank: " + school.Rank + "\nScore: " + school.Score,
+                    icon: 'Content/' + school.Icon
+                });
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+
+}
---
+++ LeedsSchoolsMap/IndexModule.fs
@@ -0,0 +1,78 @@
+﻿namespace LeedsSchoolsMap
+
+open Nancy
+open System
+open System.IO
+open Newtonsoft.Json
+
+type SchoolPre = {
+    Name: string;
+    Lat: decimal;
+    Lon: decimal;
+    Rank: float;
+    Score: float;
+    Private: bool;
+    Religious: bool;
+    Catholic: bool;
+    Type: string
+}
+
+type School = {
+    Name: string;
+    Lat: decimal;
+    Lon: decimal;
+    Rank: float;
+    Score: float;
+    Type: string;
+    Icon: string
+}
+
+type IndexModule() as x =
+    inherit NancyModule()
+
+    let sixth = (1.0/6.0)
+
+    let rankPart = function
+        | rank when rank >= sixth*5.0 -> "1"
+        | rank when rank >= sixth*4.0 -> "2"
+        | rank when rank >= sixth*3.0 -> "3"
+        | rank when rank >= sixth*2.0 -> "4"
+        | rank when rank >= sixth*1.0 -> "5"
+        | rank when rank >= 0.0 -> "6"
+        | _ -> "x" // for schools with an unknown rank
+
+    let typePart = function
+        | "Secondary" -> "s"
+        | "Primary" -> "p"
+        | x -> failwith "Unrecognised school type: " + x
+
+    let trueFalsePart = function
+        | true -> "t"
+        | false -> "f"
+
+    let religiousPart _ = "f"
+
+    let getIcon (school:SchoolPre) = 
+        sprintf "%s_%s_%s_%s_%s.png"
+            (school.Type |> typePart)
+            (school.Rank |> rankPart)
+            (school.Private |> trueFalsePart)
+            (school.Religious |> trueFalsePart)
+            (school.Catholic |> trueFalsePart)
+
+    let loadSchoolData() = 
+        File.ReadAllLines(__SOURCE_DIRECTORY__ + """\data\school_locations.csv""").[1..]
+        |> Array.map (fun (line:string) -> line.Split(','))
+        |> Array.map (fun school -> {Name = school.[0]; Lat = decimal school.[1]; Lon = decimal school.[2]; Rank = float school.[3]; Score = float school.[4]; Private = Convert.ToBoolean(school.[5]); Religious = Convert.ToBoolean(school.[6]); Catholic = Convert.ToBoolean(school.[7]); Type = school.[8] })
+        |> Array.map (fun school -> {Name = school.Name; Lat = school.Lat; Lon = school.Lon; Rank = school.Rank; Score = school.Score; Type = school.Type; Icon = school |> getIcon})
+        |> Array.toList 
+
+    do x.Get.["/"] <- fun _ -> 
+        box x.View.["index"]
+
+    do x.Get.["/schools"] <- fun _ -> 
+
+        let schoolData = loadSchoolData()
+        let json = JsonConvert.SerializeObject(schoolData)
+
+        json :> obj
